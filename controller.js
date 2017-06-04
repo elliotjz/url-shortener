33a07a3571a8f8e633b37f4690bff0109ebd3726
Elliot Zoerner <eliot.zoerner@gmail.com>
2017-06-04 10:36:09 +0200

    redirect working

--- controller.js
+++ controller.js
@@ -15,9 +15,8 @@ let UrlModel = mongoose.model('Short-urls', urlSchema);
 module.exports = function(app) {
 
 	app.get('/', function(req, res) {
-		res.sendFile(__dirname + '/index.html');
+		res.sendFile(__dirname + '/routes/index.html');
 	})
-
 
 	app.get('/new/*', function(req, res) {
 		let url = req.url.slice(5);
@@ -25,6 +24,7 @@ module.exports = function(app) {
 		let result;
 
 		if (url.match(/(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?/)) {
+
 			let countDoc = UrlModel.findOne({original_url: "counter"}, function(err, data) {
 				if (err) throw err;
 				let count = data.short_url;
@@ -46,16 +46,28 @@ module.exports = function(app) {
 			});
 
 		} else {
-			newUrl = { error: "Wrong URL format. Make sure you include the http://, https:// or ftp:// prefix." };
+			newUrl = { Error: "Wrong URL format. Make sure you include the http://, https:// or ftp:// prefix." };
 			res.json(newUrl);
 		}
 	})
 
-	app.get('*', function(req, res) {
+	app.get(/\/[0-9]/, function(req, res) {
+
 		let urlDoc = UrlModel.findOne({short_url: req.url.slice(1)}, function(err, data) {
 			if (err) throw err;
-			let url = data.original_url;
-			console.log(url);
+			if (data) {
+				let url = data.original_url;
+				res.redirect(url);
+			} else {
+				res.end("Short Url not found");
+			}
 		})
 	})
+
+	app.use(function(req, res, next) {
+	    res.status(400);
+	    res.end('404: File Not Found');
+	});
 }
+
+
